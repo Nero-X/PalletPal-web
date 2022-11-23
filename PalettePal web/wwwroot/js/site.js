6232f22fd52a7da1feb61c481c47cace71e48b97
Nero-X <SashaJCV@yandex.ru>
2022-11-23 15:36:54 +0300

    js wip

--- PalettePal web/wwwroot/js/site.js
+++ PalettePal web/wwwroot/js/site.js
@@ -1,4 +1,32 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let colorsContainer = $(".colors-list").first();
 
-// Write your JavaScript code.
+$('#open-btn').click(() => {
+    console.log("clicked")
+    $('#file-input').click();
+});
+
+$("form").submit(async function (event) {
+    event.preventDefault();
+    // TODO: test image size
+    colorsContainer.empty();
+    let response = await fetch("post", {
+        method: "ajax",
+        body: new FormData(this)
+    });
+    let result = await response.json();
+    for (let color of result.colorsList) {
+        colorsContainer.append(`<div class="color" style="background-color: #${color}"><a>#${color}</a></div>`);
+    };
+});
+
+function previewFile() {
+    const preview = document.querySelector('img');
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+        preview.src = reader.result;
+    }
+
+    if (file) reader.readAsDataURL(file);
+}
